6229bf58bf7f53a8bd9f905c9435b5fb0c9304b0
dequan300 <dequanchambers94@gmail.com>
2019-12-09 09:27:03 -0500

    update

--- connection.js
+++ connection.js
@@ -1,24 +1,34 @@
 
 // // requires the installed pg module
 // // destructured, only take part of the export from pg called Pool
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 try {
-    // When not running via Heroku, this will load the .env file.
-    require("dotenv").config();
+  // When not running via Heroku, this will load the .env file.
+  require('dotenv').config();
 } catch (e) {
-    // When running with Heroku, dotenv doesn’t need to be available.
+  // When running with Heroku, dotenv doesn't need to be available.
 }
 const connectionString = process.env.DATABASE_URL;
-
 const pool = new Pool({
-    connectionString: connectionString,
-    ssl: !connectionString.includes('localhost')
+  connectionString: connectionString,
+  ssl: !connectionString.includes('localhost')
 });
 
+module.exports = pool;
 
 
- 
+
+//   const credentials = new Pool({
+//     user: "postgres",
+//     password: "password",
+//     host: "localhost",
+//     port: 5432,
+//     database: "PlayerInfo",
+//     ssl: false
+// });
+
 
     module.exports = pool;
+    // module.exports = credentials;
 
   
--- routes.js
+++ routes.js
@@ -2,10 +2,56 @@ const express = require("express")
 const playerRoutes=express.Router();
 const pool= require("./connection");
 
-// let nextId=3
+let nextId=3
+
+playerRoutes.get("/player-info",(req,res)=>{
+let sql ="select *from player_data"
+pool.query(sql).then(result=>{
+    // console.log(result.rows)
+res.json(result.rows)
+res.status(200)
+});
+});
 
 playerRoutes.get("/player-info/:id",(req,res)=>{
 let param =[id]
 const id = parseInt(req.param.id)
+let sql = "select*from player_data where id=$1::int"
+pool.query(sql,param).then(result=>{
+    if(result.rows.lenght!==0){
+        res.json(result.rows[0])
+    }else{
+        res.status(404)
+        res.send("no cart items")
+    }
+})
 });
+
+
+
+playerRoutes.post("/player-info",(req,res)=>{
+    const info=req.body;
+    info.id =nextId
+    nextId++
+    let sql= `insert into player_data (name,popularity,charisma,academic)
+    values ($1::text,$2::int,$3::int,$4::int)`;
+    let param= [info.name,info.popularity,info.charisma,info.academic]
+    pool.query(sql,param).then(result=>{
+      res.status(201);
+      res.json(result.rows[0])
+
+    })
+    
+
+});
+playerRoutes.put("player_data/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const player= req.body;
+    player.id = id;
+    
+    const index = players.findIndex(i => i.id === id);
+    
+    players.splice(index, 1, player);
+    res.json(player);
+  });
 module.exports=playerRoutes;
